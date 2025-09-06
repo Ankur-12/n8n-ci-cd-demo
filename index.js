9dcc7c194c23f94dcae15d7da13ba4292994f512
Ankur-12 <99594685+Ankur-12@users.noreply.github.com>
2025-09-07 01:40:03 +0530

    another commit for testing

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app=express();
+const port=3000;
+
+
+
+//adding middlewares
+app.use(express.json());
+
+//get request
+app.get("/",(req,res)=>{
+    res.send("<h1>This is Heading</h1>") 
+})
+
+app.post("/car",(req,res)=>{
+    res.send("This is post route for car")
+})
+app.listen(port,()=>{
+    console.log("App chal rha hai hamara port pe ")
+})
